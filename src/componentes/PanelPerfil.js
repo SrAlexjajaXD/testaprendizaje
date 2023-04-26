e9eeb2df7c6a1d574a310a822abe37ba55f55001
SrAlexjajaXD <alexloga18@gmail.com>
2023-04-26 13:18:43 -0600

    Se empezó con el desarrollo del panel de perfil

--- src/componentes/PanelPerfil.js
+++ src/componentes/PanelPerfil.js
@@ -1,9 +1,45 @@
 import React from 'react'
+import { CiAlignLeft, CiUser, CiFaceSmile, CiFileOn } from "react-icons/ci";
+import styles from '../estilos/estilosPaneles.module.css'
 
 function PanelPerfil() {
+
+  var nombre = "Antonia"
+  var ap = "Albertson"
+  var am = "Winston"
+  var id = "1233"
+  var escuela = "Escuela Normal Urbana Federal de Cuautla"
+  var correo = "antoniaalberson@gmail.com"
+
+
+
+
   return (
     <div className='dashboard'>
-      PanelPerfil componente
+      <nav className={styles.encabezadoPanel}>
+        <div className={styles.logoXXL}>  
+          <CiUser/>
+        </div>
+        <div className={styles.encabezadoTitulos}>
+          <h1 className={styles.h1}>{nombre} {ap} {am}</h1>
+          <h3 className={styles.h3}>#{id}</h3>
+          <h5 className={styles.h5}>{escuela}</h5>
+        </div>
+      </nav>
+      <hr color='#FEDC97'/>
+        <form className={styles.panelPerfil}>
+          <input type="text" name="nombre" value={nombre} className={styles.nombreP}/>
+          <input type="text" name="ap" value={ap} className={styles.apellidoP}/>
+          <input type="text" name="am" value={am} className={styles.apellidoM}/>
+          <input type="email" name="correo" value={correo} className={styles.correo}/>
+          <input type="password" name="contrasena" value="asdfasdfasd" className={styles.contra}/>
+          <input type="text" name="escuela" value={escuela} className={styles.escuela}/>
+          <div className={styles.perfilBtns}>
+            <button>Modificar</button>
+            <button>Generar</button>
+            <button>Cerrar sesión</button>
+          </div>
+        </form>
     </div>
   )
 }
