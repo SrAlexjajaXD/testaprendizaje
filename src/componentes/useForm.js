39ff76774aceec20d108271381aebaa42be540ed
SrAlexjajaXD <alexloga18@gmail.com>
2023-05-27 10:11:29 -0600

    Se quito mensaje de error en el servidor

--- src/componentes/useForm.js
+++ src/componentes/useForm.js
@@ -33,7 +33,7 @@ export const useForm = (initialForm, validateForm) => {
         axios.get("https://nodejs-restapi-test-mysql-production.up.railway.app/login/docentes/" + form.correo).then(response => {
             setDatos(response.data)
         }).catch(function (error) {
-            Swal.fire({ icon: "error", title: "Error en el servidor", text:"Contacta a los desarrolladores para solucionar el problema" })
+            console.log("ERROR EN LA SOLICITUD", error)
         })
         // axios.get("http://localhost:3001/login/docentes/" + form.correo).then(response => {
         //     setDatos(response.data)
